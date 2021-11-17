17d6e97743a01f8ed68497b8b719f755b15723ba
Freddec <freddec.games@gmail.com>
2021-11-17 10:38:44 +0100

    dev

--- backend/api/views/_base.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from rest_framework import status
-from rest_framework import serializers
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated
-
-from libs.db import *
--- vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    devServer: {
-        host: "testspacecompany.exileng.com",
-        public: "testspacecompany.exileng.com",
-    },
-}
